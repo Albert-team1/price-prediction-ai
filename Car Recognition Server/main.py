61962e7f835c07c957e2fe6ebae9b7c022db1699
unknown <gitrepowizard@gmail.com>
2020-09-20 21:23:33 -0500

    removing database components

--- Car Recognition Server/main.py
+++ Car Recognition Server/main.py
@@ -1,25 +1,14 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
-from flask_sqlalchemy import SQLAlchemy
+#from datetime import timedelta
+#from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import numpy as np
-import tensorflow as tf
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
+#import tensorflow as tf
+
 import pickle 
 import os
 from werkzeug.utils import secure_filename
-from PIL import Image
 
-t = Image.open('static/uploads/car.jpg')
-MODEL_PATH = 'tmp/saved_cars_modelv2'
-
-image_model = load_model(MODEL_PATH)
-
-'''
-Temporary profile,m change, update, delete make
-'''
 
 UPLOAD_FOLDER = 'static/uploads/' 
 
@@ -46,6 +35,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 #app.permanent_session_lifetime = timedelta(minutes=5)
 
+'''
 db = SQLAlchemy(app)
 # close browser, session data is deleted
 class users(db.Model):
@@ -68,7 +58,7 @@ class users(db.Model):
         self.color = color
         self.condition = condition
         self.prediction = prediction
-
+'''
 @app.route("/predict/<filename>")
 def predict(filename):
     
@@ -76,14 +66,10 @@ def predict(filename):
         flash("You are not logged in!")     
         return redirect(url_for("login"))
     
-    car = Image.open('static/uploads/' + filename)
-    width = height = 224
-    size = (width, height)
-    car = car.resize(size)
-    img_array = np.array(car)/255.0
-    x = np.expand_dims(img_array, axis=0)
-    preds = image_model.predict(x)
-    result = str(labels.loc[(np.argmax(preds[0])), 'class'])
+    '''
+    PLEASE ADD TF MODEL REQUEST CODE HERE
+    '''
+ 
     user = session["user"]
     year = str(session["year"])
     make = str(session["make"])
@@ -101,13 +87,14 @@ def predict(filename):
     entry = [age, make_median_price, condition_median_price, color_median_price, make_count, condition_count, color_count]
     entry = np.array(entry)
     entry = entry.reshape(1, -1)
-    prediction = round(data_model.predict(entry)[0], 2)
+    prediction = round(data_model.predict(entry)[0])
 
+    '''
     found_user = users.query.filter_by(name=user).first()
     found_user.prediction = prediction 
     db.session.commit()
-
-    return render_template("predict.html", prediction=prediction, filename=filename, result=result)
+    '''
+    return render_template("predict.html", prediction=prediction, filename=filename)
     
 
 @app.route("/")
@@ -122,17 +109,14 @@ def login():
         user = request.form["nm"]
         session["user"] = user
 
-        found_user = users.query.filter_by(name=user).first() # query database looking for username, user return with first entry
-        if found_user:
-            session["make"] = found_user.make
-            session["model"] = found_user.model
-            session["year"] = found_user.year
-            session["color"] = found_user.color
-            session["condition"] = found_user.condition     
-        else:
-            usr = users(user, "", "", "", "", "", 0.0)   # instantiating user
-            db.session.add(usr) # staging area, not commit, roll back to previous version of database
-            db.session.commit()         
+        #found_user = users.query.filter_by(name=user).first() # query database looking for username, user return with first entry
+        #if found_user:
+          
+        #else:
+        #   pass
+        #    usr = users(user, "", "", "", "", "", 0.0)   # instantiating user
+        #    db.session.add(usr) # staging area, not commit, roll back to previous version of database
+        #    db.session.commit()         
         flash("Login Successful")       
         return redirect(url_for("user"))
     else:
@@ -142,9 +126,11 @@ def login():
             
         return render_template("login.html")
 
+'''
 @app.route("/view")
 def view():
     return render_template("view.html", values=users.query.all()) # list all the 
+'''
 @app.route("/user", methods=["POST", "GET"])
 def user():
     
@@ -166,13 +152,15 @@ def user():
             session["year"] = year
             session["condition"] = condition
             session["color"] = color
+            '''
             found_user = users.query.filter_by(name=user).first()
             found_user.make = make # updating user make
             found_user.model = model 
             found_user.year = year
             found_user.condition = condition
             found_user.color = color 
-            db.session.commit()                     
+            db.session.commit() 
+            '''                    
             flash("Car Properties were saved")
 
             file = request.files['file']
@@ -213,5 +201,5 @@ def display_image(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)    
     
 if __name__ == '__main__':
-    db.create_all()
+    #db.create_all()
     app.run(debug=True)
